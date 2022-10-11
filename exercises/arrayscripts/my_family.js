6b0149e2a2ae059ea76ed7dc2c7839486ddc0226
Eric Harris <ericoldie@gmail.com>
2022-10-11 15:56:56 -0400

    array exercises

---
+++ exercises/arrayscripts/my_family.js
@@ -0,0 +1,8 @@
+myFamily = ['Chris', 'Anthony', 'Bryan', 'Diamond']
+let familyLength = myFamily.length
+let i;
+
+for (i = 0; i < familyLength; i++) {
+    const family = myFamily[i];
+    console.log(family);
+}
